7824106804416d7bdbc7e9d7467c864dbbd78d60
Rey <quit9909@icloud.com>
2015-05-04 14:41:20 +0900

    Initial Commit

---
+++ MyGroupApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGroupApp
+//
+//  Created by ReyMasuda on 2015/05/04.
+//  Copyright (c) 2015年 ReyMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
